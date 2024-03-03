94f58117d6345adfb4f79fd667ea8cf91808477b
Mathys Henneron <henneron.mathyss@gmail.com>
2024-03-03 19:42:54 +0100

    Ajout des Cards pour la page Home

---
+++ src/components/Cards/Card.jsx
@@ -0,0 +1,11 @@
+function Card({ id, cover, title }) {
+  return (
+    <li id={id} className="card">
+      <img src={cover} alt={cover} />
+      <p>{title}</p>
+      <div className="gradient"></div>
+    </li>
+  );
+}
+
+export default Card;
---
+++ src/components/Cards/CardsLogements.jsx
@@ -0,0 +1,17 @@
+import tableau from "../../datas/logements.json";
+import Card from "./Card";
+import { Link } from "react-router-dom";
+
+function CardsLogements() {
+  return (
+    <ul className="box_fiches">
+      {tableau.map((data) => (
+        <Link to={"/logement/" + data.id} key={data.id}>
+          <Card cover={data.cover} title={data.title} logementId={data.id} />
+        </Link>
+      ))}
+    </ul>
+  );
+}
+
+export default CardsLogements;
