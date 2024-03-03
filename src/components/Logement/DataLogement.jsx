f18fcbe8c563200b33090ff6d0bbc15d9cffb0c4
Mathys Henneron <henneron.mathyss@gmail.com>
2024-03-03 19:40:41 +0100

    Ajout des datas pour la pages Logement

---
+++ src/components/Logement/DataLogement.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Carousel from "../Carousel";
+import Collapsible from "../Collapsible";
+import Rating from "../Rating";
+
+function DataLogement({
+  pictures,
+  title,
+  location,
+  rating,
+  tags,
+  description,
+  equipments,
+  host,
+}) {
+  return (
+    <div className="logement">
+      <Carousel data={pictures} />
+
+      <div className="logement_flex">
+        <div className="logement_title">
+          <h1>{title}</h1>
+          <h2>{location}</h2>
+          <div className="logement_tag_flex">
+            {tags.map((tag, index) => (
+              <li key={index} className="logement_li_flex">
+                {tag}
+              </li>
+            ))}
+          </div>
+        </div>
+        <div className="logement_host">
+          <span className="logement_host_flex">
+            <p align="right">{host.name}</p>
+            <img
+              className="logement_host_round"
+              src={host.picture}
+              alt={host.name}
+            />
+          </span>
+          <Rating data={rating} />
+        </div>
+      </div>
+
+      <div className="logement_desc">
+        <div className="logement_desc_col margin">
+          <Collapsible label="Description">
+            <p className="desc_p">{description}</p>
+          </Collapsible>
+        </div>
+        <div className="logement_desc_col">
+          <Collapsible label="Équipements">
+            <p className="desc_p">
+              {equipments.map((equipment, index) => (
+                <li key={index}>{equipment}</li>
+              ))}
+            </p>
+          </Collapsible>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DataLogement;
---
+++ src/components/Logement/Logement.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import tableau from "../../datas/logements.json";
+import DataLogement from "./DataLogement";
+import { useParams } from "react-router-dom";
+import Error from "../Error";
+
+function Logement() {
+  const { logementId } = useParams();
+  const logement = tableau.find((logement) => {
+    return logement.id === logementId;
+  });
+  if (!logement) {
+    return <Error />;
+  }
+
+  return <DataLogement {...logement} />;
+}
+
+export default Logement;
